8122fc8c0a78da226210f06869bb5fafd5776fd7
Zoheir Khonyagar <zoheirkhonyagar@gmail.com>
2020-03-15 14:53:18 +0330

    setup express server

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,1 +1,17 @@
-console.log('I am here');
+import express from 'express';
+import { json } from 'body-parser';
+
+// initial express app
+const app = express();
+
+// add middleware to
+app.use(json());
+
+// reserve port to listen
+app.listen(3000, (err: Error) => {
+  if (err) {
+    throw Error;
+  }
+
+  console.log('server is running on port 3000');
+});
