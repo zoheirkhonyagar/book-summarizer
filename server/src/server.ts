dc3c4fd6811c4278b84bd1c3b02672f0cc149d2d
Zoheir Khonyagar <zoheirkhonyagar@gmail.com>
2020-03-15 15:24:12 +0330

    setup graphql in express server

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,27 @@
 import express from 'express';
+import graphqlHTTP from 'express-graphql';
 import { json } from 'body-parser';
+import { buildSchema } from 'graphql';
 
 // initial express app
 const app = express();
 
+// create simple schema
+const schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+
 // add middleware to
 app.use(json());
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema,
+    graphiql: true
+  })
+);
 
 // reserve port to listen
 app.listen(3000, (err: Error) => {
