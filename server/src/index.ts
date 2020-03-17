6f82900c6da84975d73d80cd3381e6b8ec060453
Zoheir Khonyagar <zoheirkhonyagar@gmail.com>
2020-03-18 01:12:00 +0330

    run graphql and express

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,21 +1,49 @@
-import "reflect-metadata";
-import {createConnection} from "typeorm";
-import {User} from "./entity/User";
+import 'reflect-metadata';
+import express from 'express';
+import { ApolloServer } from 'apollo-server-express';
+// import { createConnection } from 'typeorm';
+// import { User } from './entity/User';
 
-createConnection().then(async connection => {
+(async () => {
+  // initial express app
+  const app = express();
 
-    console.log("Inserting a new user into the database...");
-    const user = new User();
-    user.firstName = "Timber";
-    user.lastName = "Saw";
-    user.age = 25;
-    await connection.manager.save(user);
-    console.log("Saved a new user with id: " + user.id);
+  app.get('/', (_req, res) => res.send('hello world'));
 
-    console.log("Loading users from the database...");
-    const users = await connection.manager.find(User);
-    console.log("Loaded users: ", users);
+  const apolloServer = new ApolloServer({
+    typeDefs: `
+      type Query {
+        hello: String!
+      }
+    `,
+    resolvers: {
+      Query: {
+        hello: () => 'hello world'
+      }
+    }
+  });
 
-    console.log("Here you can setup and run express/koa/any other framework.");
+  apolloServer.applyMiddleware({ app });
 
-}).catch(error => console.log(error));
+  app.listen(4000, () => {
+    console.log('server running on port 4000');
+  });
+})();
+
+// createConnection().then(async connection => {
+
+//     console.log("Inserting a new user into the database...");
+//     const user = new User();
+//     user.firstName = "Timber";
+//     user.lastName = "Saw";
+//     user.age = 25;
+//     await connection.manager.save(user);
+//     console.log("Saved a new user with id: " + user.id);
+
+//     console.log("Loading users from the database...");
+//     const users = await connection.manager.find(User);
+//     console.log("Loaded users: ", users);
+
+//     console.log("Here you can setup and run express/koa/any other framework.");
+
+// }).catch(error => console.log(error));
