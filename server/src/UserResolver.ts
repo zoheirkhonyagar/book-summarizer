8a5bfa81b20f703a25f51dfc96af3b6b78802d04
Zoheir Khonyagar <zoheirkhonyagar@gmail.com>
2020-03-18 02:29:02 +0330

    add users query

--- server/src/UserResolver.ts
+++ server/src/UserResolver.ts
@@ -9,14 +9,25 @@ export class UserResolver {
     return 'hi';
   }
 
+  @Query(() => [User])
+  async users() {
+    try {
+      return await User.find({});
+    } catch (error) {
+      return error;
+    }
+  }
+
   @Mutation(() => Boolean)
   async register(
     @Arg('email') email: string,
     @Arg('password') password: string
   ) {
+    // create hashed password
     const hashedPassword = await hash(password, 12);
 
     try {
+      // create user
       await User.insert({
         email,
         password: hashedPassword
--- server/src/entity/User.ts
+++ server/src/entity/User.ts
@@ -1,10 +1,14 @@
 import { Entity, ObjectIdColumn, ObjectID, Column, BaseEntity } from 'typeorm';
+import { ObjectType, Field, ID } from 'type-graphql';
 
+@ObjectType()
 @Entity('users')
 export class User extends BaseEntity {
+  @Field(() => ID)
   @ObjectIdColumn()
   id: ObjectID;
 
+  @Field()
   @Column()
   email: string;
 
