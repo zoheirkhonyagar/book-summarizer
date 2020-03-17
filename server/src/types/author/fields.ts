5ab22489a91657aa54116fba36fd227c3b861c70
Zoheir Khonyagar <zoheirkhonyagar@gmail.com>
2020-03-17 03:37:14 +0330

    add addAuthor mutation

--- server/src/types/author/fields.ts
+++ server/src/types/author/fields.ts
@@ -1,3 +1,4 @@
+import { GraphQLList, GraphQLString, GraphQLInt } from 'graphql';
 import { Field } from '../../interfaces/field';
 import { authorType } from './author';
 import Author from './../../models/author';
@@ -5,17 +6,39 @@ import Author from './../../models/author';
 // define queries
 // define authors field
 const authors: Field = {
-  type: authorType,
+  type: GraphQLList(authorType),
   resolve(parent, args) {
     return Author.find({});
   }
 };
 
 // define mutations
+// define add author
+const addAuthor: Field = {
+  type: authorType,
+  args: {
+    name: { type: GraphQLString },
+    age: { type: GraphQLInt }
+  },
+  resolve(parent, args) {
+    // execute params
+    const { name, age } = args;
+
+    // create an author instance
+    const author = new Author({
+      name,
+      age
+    });
+
+    return author.save();
+  }
+};
 
 export default {
   query: {
     authors
   },
-  mutation: {}
+  mutation: {
+    addAuthor
+  }
 };
